5e3ae1e02fb4fa714caea63ea22f9848d6910e48
QU35TMJ <maureenjune16@gmail.com>
2020-11-17 15:22:41 +0300

    installed uniqid and styled app.js

--- src/App.js
+++ src/App.js
@@ -2,46 +2,55 @@ import React, { Component } from "react";
 import Overview from "./components/Overview";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
+
     this.state = {
       task: "",
       tasks: [],
     };
   }
 
-  handleChange = (e) =>{//sets the current task in state to whatever the user types in the input field
+  handleChange = (e) => {
     this.setState({
       task: e.target.value,
     });
   };
 
-  onSubmitTask = (e) =>{
-    e.preventDefault(); //prevent refreshing of the form after submission
+  onSubmitTask = (e) => {
+    e.preventDefault();
     this.setState({
-      tasks: this.state.tasks.concat(this.state.task),//adds the tast in the input field to the task array
+      tasks: this.state.tasks.concat(this.state.task),
       task: "",
     });
   };
-  render(){
-    //eslint-disable-next-line
-    const {task,tasks} = this.state;
-    return(
-      <div className ="col-6 mx-auto mt-5">
-       <form onSubmit={this.onSubmitTask}>
-        <div className ="form-group">
-          <label htmlFor="taskInput">Enter Task</label>
-          <input onChange={this.handleChange} value={this.state.task} type="text" id="taskInput" className="form-control" />
-        </div>
-        <div className="form-group">
-          <button type="submit" className="btn btn-primary">Add Task</button>
-        </div>
 
-       </form>
-       <Overview tasks={tasks}/>
+  render() {
+    const { task, tasks } = this.state;
+
+    return (
+      <div className="col-6 mx-auto mt-5">
+        <form onSubmit={this.onSubmitTask}>
+          <div className="form-group">
+            <label htmlFor="taskInput">Enter task</label>
+            <input
+              onChange={this.handleChange}
+              value={task}
+              type="text"
+              id="taskInput"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Add Task
+            </button>
+          </div>
+        </form>
+
+        <Overview tasks={tasks} />
       </div>
-    ) ;
+    );
   }
 }
 
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,16 +2,15 @@ import React from "react";
 import uniqid from "uniqid";
 
 const Overview = (props) => {
-    const {tasks} = props;
+  const { tasks } = props;
 
-    return (
-        <ul>
-            {tasks.map((task)=> {//NB:React always requires you to add a unique key to each element when you map over a list.
-                return <li key={uniqid()}>{task}</li>;
-            }
-            )}
-        </ul>
-    );
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li key={uniqid()}>{task}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default Overview;
